9386a5e62b3f53b62c70799ba90cc57a1557f7b7
virkann <virkan90@gmail.com>
2023-12-04 20:13:03 +0300

    map categories

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { STATUS } from "../../utils/status";
 import "./Category.scss";
 import { Link } from "react-router-dom";
-import Error from '../Error/Error';
+import Error from "../Error/Error";
 import Loader from "../Loader/Loader";
 
 const Category = ({ categories, status }) => {
@@ -13,7 +13,24 @@ const Category = ({ categories, status }) => {
       <div className="container">
         <div className="categories-content">
           <div className="section-title">
+            <h3 className="text-uppercase fw-7 text-regal-blue ls-1">
+              Category
+            </h3>
+          </div>
 
+          <div className="category-item grid">
+            {
+              categories.slice(0, 5).map(category => (
+                <Link to={`category/${category.id}`} key={category.id}>
+                  <div className="category-item-img">
+                    <img src={category.image} alt="" />
+                  </div>
+                  <div className="category-item-name text-center">
+                    <h6 className="fs-20">{category.name}</h6>
+                  </div>
+                </Link>
+              ))
+            }
           </div>
         </div>
       </div>
