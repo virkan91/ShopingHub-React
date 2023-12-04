65998664145ae75382117a4e8fa4ce29c0277056
virkann <virkan90@gmail.com>
2023-12-04 10:04:09 +0300

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+import Home from "./HomePage/HomePage";
+import CategoryPage from "./CategoryPage/CategoryPage";
+import CartPage from "./CartPage/CartPage";
+
+export {Home, CategoryPage, CartPage}
